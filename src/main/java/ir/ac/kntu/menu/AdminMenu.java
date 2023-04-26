d121272ec4c2d7c2ced76e377ad4ab1747ceefe4
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-04-26 15:15:38 +0330

    add admin menu

--- src/main/java/ir/ac/kntu/menu/AdminMenu.java
+++ src/main/java/ir/ac/kntu/menu/AdminMenu.java
@@ -4,6 +4,10 @@ public class AdminMenu extends Menu {
 
     @Override
     void showMenu() {
-
+        System.out.println("-------- Admin Menu ---------");
+        System.out.println("Chose Option : ");
+        System.out.println("1- games");
+        System.out.println("2- users");
+        System.out.println("3-  Exit");
     }
 }
