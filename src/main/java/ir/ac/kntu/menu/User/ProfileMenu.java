7b7fc30adae28da6f5102d518eab9c22dd611283
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-05-01 23:10:40 +0330

    add try option for change

--- src/main/java/ir/ac/kntu/menu/User/ProfileMenu.java
+++ src/main/java/ir/ac/kntu/menu/User/ProfileMenu.java
@@ -62,7 +62,7 @@ public class ProfileMenu extends Menu {
         System.out.println("Enter your new username:");
         String newUsername = Scan.getLine().toUpperCase().trim();
         TerminalColor.red();
-        if (newUsername.length() < 3){
+        if (newUsername.length() < 3) {
             System.out.println("Username must be 3  or more character!");
             TerminalColor.reset();
             return;
@@ -86,14 +86,16 @@ public class ProfileMenu extends Menu {
 
     public void changeEmail() {
         System.out.println("Enter your new email:");
-        String newEmail = Scan.getLine().toUpperCase().trim();
+        String newEmail = Scan.getLine().toLowerCase().trim();
         TerminalColor.red();
         if (!newEmail.matches(".*@.*")) {
             System.out.println("New email is not valid!");
+            TerminalColor.reset();
             return;
         }
         if (newEmail.equals(user.getEmail())) {
             System.out.println("New email is same as previous email !");
+            TerminalColor.reset();
             return;
         }
         user.setEmail(newEmail);
@@ -124,15 +126,16 @@ public class ProfileMenu extends Menu {
         System.out.println("how much you want amount ? (1+ $)");
         String amount = Scan.getLine();
         TerminalColor.red();
-        int errorCount =0;
-        while (!amount.matches("[0-9.]+") || Double.parseDouble(amount) < 1){
+        int errorCount = 0;
+        while (!amount.matches("[1-9][0-9.]+") || Double.parseDouble(amount) < 1) {
             errorCount++;
-            System.out.println("Invalid amount ! Try again :");
-            amount = Scan.getLine();
-            if (errorCount > 2){
+            if (errorCount > 2) {
                 System.out.println("Charge wallet fail !");
                 return;
             }
+            System.out.println("Invalid amount ! Try again :");
+            amount = Scan.getLine();
+
         }
         user.chargeWallet(Double.parseDouble(amount));
         TerminalColor.green();
@@ -142,25 +145,27 @@ public class ProfileMenu extends Menu {
 
     public void changePassword() {
         int error = 0;
-        while (error < 3){
+        while (error < 3) {
             System.out.println("Enter old password:");
             String oldPassword = Scan.getLine().trim();
             System.out.println("Enter new password:");
             String newPassword = Scan.getLine().trim();
-            if (newPassword.length() <8){
+            if (newPassword.length() < 8) {
                 TerminalColor.red();
                 System.out.println("New password must be 8 or more character");
                 TerminalColor.reset();
-                return;
-            }
-            if (user.setNewPassword(newPassword, oldPassword)){
+            } else if (user.setNewPassword(newPassword, oldPassword)) {
                 TerminalColor.green();
                 System.out.println("Password successfully !");
                 TerminalColor.reset();
                 return;
+            } else {
+                TerminalColor.red();
+                System.out.println("Old password is not correct!");
+                TerminalColor.reset();
             }
             TerminalColor.red();
-            System.out.println("Fail change password ! Try again");
+            System.out.println("Fail change password !");
             TerminalColor.reset();
             error++;
         }
